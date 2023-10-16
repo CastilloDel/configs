e8d07979b304ba8715a647281f0a8d68c5515a91
CastilloDel <delcastillodelarosadaniel@gmail.com>
2023-10-16 09:10:02 +0200

    Add helix config

--- setup.fish
+++ setup.fish
@@ -2,6 +2,7 @@
 
 mkdir -p ~/.config/alacritty && cp alacritty.yml ~/.config/alacritty/alacritty.yml
 mkdir -p ~/.config/zellij && cp zellij.kdl ~/.config/zellij/config.kdl
+mkdir -p ~/.config/helix && cp helix.toml ~/.config/helix/config.toml
 mkdir -p ~/.config/fish && cp config.fish ~/.config/fish/config.fish
 mkdir -p ~/.config/nvim && cp init.vim ~/.config/nvim/init.vim
 cp starship.toml ~/.config/starship.toml
