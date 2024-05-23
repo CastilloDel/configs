28bb98f3095b3a80391d1106af183b03a939cb8f
CastilloDel <delcastillodelarosadaniel@gmail.com>
2024-05-23 16:08:48 +0200

    Update alacritty config to be a toml

--- setup.fish
+++ setup.fish
@@ -1,6 +1,6 @@
 #!/usr/bin/fish
 
-mkdir -p ~/.config/alacritty && cp alacritty.yml ~/.config/alacritty/alacritty.yml
+mkdir -p ~/.config/alacritty && cp alacritty.toml ~/.config/alacritty/alacritty.toml
 mkdir -p ~/.config/zellij && cp zellij.kdl ~/.config/zellij/config.kdl
 mkdir -p ~/.config/helix && cp helix.toml ~/.config/helix/config.toml && cp helix_languages.toml ~/.config/helix/languages.toml
 mkdir -p ~/.config/fish && cp config.fish ~/.config/fish/config.fish
